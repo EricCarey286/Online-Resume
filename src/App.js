0a2cddbd5c680f0b9770163470d6d93f8da8ae46
Eric Carey <ericwcarey@gmail.com>
2024-01-10 17:42:57 +0000

    Added Contact form with EmailJs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ConceptList from "./components/Concepts/ConceptList";
 import Jobs from "./components/Jobs/Jobs";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header image={headshot} />
       <ConceptList />
       <Jobs />
+      <Contact />
       <Footer />
     </>
   );
